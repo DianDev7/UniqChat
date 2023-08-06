a6181401a37450b8cd155fb38f778fdb233f6913
DianDev7 <DianDev4712@gmail.com>
2023-08-06 13:25:00 +0200

    Delete project

--- UniqChat/UniqChat/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UniqChat.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
